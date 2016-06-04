f5e6483c304cefecd12c3658a0a5a3222978c89e
justin94lee <justinleeig@gmail.com>
2016-06-04 22:41:13 +0800

    edited this

--- 9_Magic_Methods_Properties_and_Iterators.py
+++ 9_Magic_Methods_Properties_and_Iterators.py
@@ -11,25 +11,44 @@ super 函数只在新式类中起作用
 
 ''' 
 
-__metaclass__ = type
-class Bird:
-    def __init__(self):
-        self.hungry = True
-    
-    def eat(self):
-        if self.hungry:
-            print "Ahhh"
-            self.hungry =False
-            
-        else:
-            print 'No thanks! '
-class SongBird(Bird):
-    def __init__(self):
-        super(SongBird, self).__init__()
-        self.sound = 'Squaek'
-    def sing(self):
-        print self.sound
-        
-sb = SongBird()
-sb.sing()
-sb.eat()
+#__metaclass__ = type
+#class Bird:
+#    def __init__(self):
+#        self.hungry = True
+#    
+#    def eat(self):
+#        if self.hungry:
+#            print "Ahhh"
+#            self.hungry =False
+#            
+#        else:
+#            print 'No thanks! '
+#class SongBird(Bird):
+#    def __init__(self):
+#        super(SongBird, self).__init__()
+#        self.sound = 'Squaek'
+#    def sing(self):
+#        print self.sound
+#        
+#sb = SongBird()
+#sb.sing()
+#sb.eat()
+
+###############################################################################
+
+'''
+# 面向对象的高级编程
+
+## 使用__slots__ 限制class能添加的属性
+
+# property 
+@property 装饰器把方法变成属性调用
+
+## 装饰器
+
+
+
+
+
+
+
